b0c18fadcb83b9e80783f4a00df17d0e8c930ccc
Bailey <baileywyoung@gmail.com>
2023-02-20 13:45:29 -0800

    Added drag to change box colors

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-const container = document.querySelector('#container')
+const container = document.querySelector('#container');
+let isMouseDown = false;
+container.onmousedown = () => isMouseDown = true;
+container.onmouseup = () => isMouseDown = false;
+
+function colorChange(target) {
+    // Making the box change color temporarily when hovered
+    target.addEventListener('mouseover', function() {target.classList.add('class', 'hoverin')});
+    target.addEventListener('mouseout', function() {target.classList.remove('class', 'hoverin')});
+    // Making the box permanently change when clicked
+    target.addEventListener('click', function() {target.setAttribute('id','clicked')});
+    // Making the box permanently change when dragged
+    target.addEventListener('mouseover', function () {if (isMouseDown) {target.setAttribute('id','clicked')}})
+}
+
 function addBlocks(number,parent) {
     for (let i = 0; i < number; i++){
         const box = document.createElement('div');
         box.classList.add('box');
         parent.appendChild(box);
-        box.setAttribute('id', `column${i + 1}`);
+        colorChange(box);
     }
 }
 function addRows(number) {
@@ -12,9 +26,9 @@ function addRows(number) {
         const row = document.createElement('div');
         row.classList.add('row');
         container.appendChild(row);
-        row.setAttribute('id', `row${i + 1}`);
-        addBlocks(number,row)
+        addBlocks(number,row);
     }
 }
 
+// Add a square grid
 addRows(20);
