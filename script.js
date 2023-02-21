91b39f2ccd1a1a823a92a482591bc209b39607a7
Bailey <baileywyoung@gmail.com>
2023-02-20 17:10:34 -0800

    Added color selection

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
+const body = document.querySelector('body');
 const container = document.querySelector('#container');
+const rows = document.querySelectorAll('.row');
+const generateButton = document.querySelector('#generate'); 
+const colorButtons = document.querySelectorAll('#color');
+let slider = document.getElementById('myRange');
+let sliderOutput = document.getElementById('sliderValue');
+let color = 'black';
+
+// Updates grid size everytime you slide
+sliderOutput.innerHTML = `Grid Size ${slider.value} x ${slider.value}`;
+slider.oninput = function() {
+    sliderOutput.innerHTML = `Grid Size ${this.value} x ${this.value}`;
+}
+generateButton.onclick = () => generateGrid(sliderOutput);
+
 let isMouseDown = false;
 container.onmousedown = () => isMouseDown = true;
 container.onmouseup = () => isMouseDown = false;
 
+const black = document.querySelector('#default');
+black.onclick = () => color = 'black';
+const eraser = document.querySelector('#eraser');
+eraser.onclick = () => color = 'white';
+const rainbow = document.querySelector('#rainbow');
+rainbow.onclick = () => color = rgbColor();
+
 function colorChange(target) {
-    // Making the box change color temporarily when hovered
-    target.addEventListener('mouseover', function() {target.classList.add('class', 'hoverin')});
-    target.addEventListener('mouseout', function() {target.classList.remove('class', 'hoverin')});
     // Making the box permanently change when clicked
-    target.addEventListener('click', function() {target.setAttribute('id','clicked')});
+    target.addEventListener('click', function() {target.style.cssText = `background-color: ${color};`});
     // Making the box permanently change when dragged
-    target.addEventListener('mouseover', function () {if (isMouseDown) {target.setAttribute('id','clicked')}})
+    target.addEventListener('mouseover', function () {if (isMouseDown) {target.style.cssText = `background-color: ${color};`}})
 }
 
 function addBlocks(number,parent) {
@@ -30,5 +49,16 @@ function addRows(number) {
     }
 }
 
-// Add a square grid
-addRows(20);
+function generateGrid(number) {
+    document.getElementById('container').innerHTML = '';
+    number = slider.value;
+    addRows(number);
+}
+
+function rgbColor() {
+    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255);
+    const b = randomBetween(0, 255);
+    return `rgb(${r},${g},${b})`; // Collect all to a css color string
+}
